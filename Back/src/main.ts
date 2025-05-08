47d10291c5da57e28a69de49da91d7eaa85371f3
lilieth <liliethramirez91@gmail.com>
2025-05-08 12:30:18 -0300

    Modifique variable de entorno

--- Back/src/main.ts
+++ Back/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: process.env.FRONT_URL || 'http://localhost:3001',
     credentials: true,
   });
 
